18c8edfea003adb27260808122f8758fdc72b547
Agustina Loureiro <121360463+AgustinaLoureiro@users.noreply.github.com>
2025-07-02 00:13:53 -0300

    Prueba

--- Clase 19 114-1/Configuracion.py
+++ Clase 19 114-1/Configuracion.py
@@ -4,6 +4,8 @@ from Funciones import *
 
 pygame.init()
 
+#holaJaz
+
 boton_suma = crear_elemento_juego("mas.webp",60,60,420,200)
 boton_resta = crear_elemento_juego("menos.webp",60,60,20,200)
 boton_volver = crear_elemento_juego("textura_respuesta.jpg",100,40,10,10)
